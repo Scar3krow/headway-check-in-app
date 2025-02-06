7a9a1a8f36033f5975cbb2710aecba0f1b0168ea
James Page <james.page002@gmail.com>
2025-02-06 17:45:22 +0800

    added correct code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'; 
+import { BrowserRouter as Router, Route, Routes, Navigate, useLocation, useNavigate } from 'react-router-dom'; 
 import Register from './components/Register'; 
 import Login from './components/Login';
 import Home from './components/Home';
